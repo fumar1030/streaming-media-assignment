cb76ac98ccdb60862893592914d76f3c41ef92af
fau8135@rit.edu <fau8135@rit.edu>
2025-02-10 03:41:08 -0500

    Switching to laptop to do tests

--- src/server.js
+++ src/server.js
@@ -6,10 +6,20 @@ const port = process.env.port || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
   console.log(request.url);
+
+  switch(request.url){
+    case '/':
+      htmlHandler.getIndex(response, request);  
+      break;
+    case '/party.mp4':
+      mediaHandler.getParty(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(response, request);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
   console.log(`Listening on 127.0.0.1:${port}`);
 });
 
-/
