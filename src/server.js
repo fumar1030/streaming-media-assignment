1c1b0d9b0000a2f72322a7f55f98f9e46c31fada
fumar1030 <fau8135@rit.edu>
2025-02-10 02:20:59 -0500

    Moving code to work on other computer

--- src/server.js
+++ src/server.js
@@ -4,3 +4,12 @@ const mediaHandler = require('./mediaResponse');
 
 const port = process.env.port || process.env.NODE_PORT || 3000;
 
+const onRequest = (request, response) => {
+  console.log(request.url);
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
+
+/
