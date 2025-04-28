7d4b046ec243acb275080943ca6102a1bc41ada5
fumar1030 <92736442+fumar1030@users.noreply.github.com>
2025-04-28 15:32:28 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const htmlHandler = require('./htmlResponse');
 const mediaHandler = require('./mediaResponse');
 
-const port = process.env.port || process.env.NODE_PORT || 3000;
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
   console.log(request.url);
